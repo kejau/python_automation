7cb620f174b3704e059340b5333c4be950cc0077
kejau <kejau@hotmail.com>
2020-03-11 17:33:31 +0000

    csv

---
+++ data/generate_report.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import csv
+
+
+def read_employees(csv_file_location):
+    csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
+    employee_file = csv.DictReader(
+        open(csv_file_location), dialect='empDialect')
+    employee_list = []
+    for data in employee_file:
+        employee_list.append(data)
+        return employee_list
+
+employee_list = read_employees('<file_location>')
+print(employee_list)
